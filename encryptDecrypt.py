c8577d1f42a89003510bdd143efbeab0e461c575
Daraksha Afreen <86185881+daraksha23@users.noreply.github.com>
2022-01-07 21:00:31 +0530

    Updated version

--- encryptDecrypt.py
+++ encryptDecrypt.py
@@ -1,4 +1,4 @@
-def machine():
+def machine(message, mode):
     # creating key strings
     keys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !'
     # auto generating the vaules of strings
@@ -7,16 +7,12 @@ def machine():
     values = keys[-1] + keys[0:-1]
 
     # creating two dictionaries
-    encrytDict = dict(zip(keys, values))
+    encryptDict = dict(zip(keys, values))
     decryptDict = dict(zip(values, keys))
-
-    # user input
-    message = input("Enter your secret message: ")
-    mode = input("Crypto Mode : Encode(E) OR Decode(D)")
 
     #encode and decode
     if mode.upper() == 'E':
-        newMessage = ''.join([encrytDict[letter]
+        newMessage = ''.join([encryptDict[letter]
                               for letter in message])
     elif mode.upper() == 'D':
         newMessage = ''.join([decryptDict[letter]
@@ -25,6 +21,3 @@ def machine():
         print("Please try again, wrong choice entered")
 
     return newMessage
-
-
-print(machine())
